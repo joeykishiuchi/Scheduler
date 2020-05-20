e552324f97b0ef6e06b94d10b57493d7f8fc0bb6
Joey Kishiuchi <josephkishiuchi@outlook.com>
2020-05-20 16:24:07 -0700

    Change exiting from error message will reset to original Empty or Show mode

--- src/components/Appointment/Index.js
+++ src/components/Appointment/Index.js
@@ -1,66 +1,69 @@
-import React, { useEffect } from "react"
+import React, { useEffect } from "react";
 
-import Header from "./Header"
-import Show from "./Show"
-import Empty from "./Empty"
-import Form from "./Form"
-import Status from "./Status"
-import Confirm from "./Confirm"
-import Error from "./Error"
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+import Form from "./Form";
+import Status from "./Status";
+import Confirm from "./Confirm";
+import Error from "./Error";
 
-import "./styles.scss"
+import "./styles.scss";
 
-import useVisualMode from "hooks/useVisualMode"
+import useVisualMode from "hooks/useVisualMode";
 
 export default function Appointment(props) {
   
-  // MODES
-  const EMPTY = "EMPTY";
-  const SHOW = "SHOW";
-  const CREATE = "CREATE";
-  const SAVE = "SAVE"
-  const DELETE = "DELETE"
-  const CONFIRM = "CONFIRM"
-  const EDIT = "EDIT"
-  const ERROR_SAVE = "ERROR_SAVE"
-  const ERROR_DELETE = "ERROR_DELETE"
+// Various mode options for a given hour
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+const SAVE = "SAVE";
+const DELETE = "DELETE";
+const CONFIRM = "CONFIRM";
+const EDIT = "EDIT";
+const ERROR_SAVE = "ERROR_SAVE";
+const ERROR_DELETE = "ERROR_DELETE";
 
- const {mode, transition, back} = useVisualMode(
-   props.interview ? SHOW : EMPTY
- )
-
- useEffect(() => {
+const {mode, transition, back} = useVisualMode(
+  props.interview ? SHOW : EMPTY 
+);
+// Initializes appointment mode on page refresh
+useEffect(() => {
   if (props.interview === null && mode === "SHOW") {
     transition(EMPTY);
   }
   if (props.interview && mode === "EMPTY") {
     transition(SHOW);
   }
- }, [ props.interview, mode, transition ]);
+}, [ props.interview, mode, transition ]);
 
- function save(name, interviewer) {
+function save(name, interviewer) {
   const interview = {
     student: name,
     interviewer
   };
   transition(SAVE, true)
   props.bookInterview(props.id, interview)
-  .then(() => transition(SHOW)) 
-  .catch(() => transition(ERROR_SAVE));
-}
+    .then(() => transition(SHOW)) 
+    .catch(() => transition(ERROR_SAVE, true)); // Displays appropriate error message if saving fails in the server API
+};
 
 function deleteInt() {
   transition(DELETE, true);
   props.deleteInterview(props.id)
-  .then(() => transition(EMPTY))
-  .catch(() => transition(ERROR_DELETE, true));
-}
+    .then(() => transition(EMPTY))
+    .catch(() => transition(ERROR_DELETE, true)); // Displays appropriate error message if delete fails in the server API
+};
 
   return (
-     <article className="appointment">
+     <article 
+      className="appointment"
+      data-testid="appointment"
+     >
        <Header time={ props.time } />
        {mode === EMPTY && <Empty onAdd={ () => transition(CREATE) } />}
-       {mode === SHOW && (
+       {mode === SHOW && props.interview && (
           <Show
             student={props.interview.student}
             interviewer={props.interview.interviewer}
@@ -95,4 +98,4 @@ function deleteInt() {
         {mode === ERROR_DELETE && <Error message="Could not delete appointment." onClose={() => back()}/>}
      </article>
   );  
-}
+};
